74dbbf263e38c72fd8eed902f1e4751061406ffe
Ezequiel <ecohort@fjsolutions.com.ar>
2022-09-15 11:39:19 -0300

    First commit

---
+++ build/precache-manifest.85af6842d7442b27e0d17cbd27187b66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fc8673aee09404fe53ef630932199e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53883f528113fc10b0d",
+    "url": "/static/css/main.f2df2c9d.chunk.css"
+  },
+  {
+    "revision": "5bdd75304d67fec76536",
+    "url": "/static/js/2.aa512933.chunk.js"
+  },
+  {
+    "revision": "d53883f528113fc10b0d",
+    "url": "/static/js/main.89048ffa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
